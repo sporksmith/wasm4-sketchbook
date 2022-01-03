092e913961528a92a24d4014cc273c55622fdff6
Jim Newsome <jim@sporksmith.net>
2022-01-03 12:19:47 -0600

    works

--- particles/src/main.zig
+++ particles/src/main.zig
@@ -13,7 +13,7 @@ pub const panic = util.panic;
 pub const log_level: std.log.Level = .warn;
 pub const log = util.log;
 
-var particles: Particles(50) = undefined;
+var particles: Particles(200) = undefined;
 
 export fn start() void {
     platform.PALETTE.* = [_]u32{ 0xfbf7f3, 0xe5b083, 0x426e5d, 0x20283d };
@@ -23,6 +23,7 @@ export fn start() void {
 
 export fn update() void {
     frame_count += 1;
+    particles.update();
     particles.draw();
 }
 
@@ -31,33 +32,35 @@ fn Particles(comptime n: u32) type {
         const Self = @This();
         const n = n;
 
-        xs: [n]u8,
-        ys: [n]u8,
-
-        fn create() Self {
-            var p = Self{
-                .xs = undefined,
-                .ys = undefined,
-            };
-            var i: u8 = 0;
-            while (i < Self.n) : (i += 1) {
-                p.xs[i] = i * 2;
-            }
-            i = 0;
-            while (i < Self.n) : (i += 1) {
-                p.ys[i] = i * 3;
-            }
-            return p;
-        }
+        xs: [n]i16,
+        ys: [n]i16,
+        spxs: [n]i16,
+        spys: [n]i16,
+        vxs: [n]i8,
+        vys: [n]i8,
 
         fn init(self: *Self) void {
             var i: u8 = 0;
             while (i < Self.n) : (i += 1) {
-                self.xs[i] = i * 2;
+                self.xs[i] = platform.CANVAS_SIZE / 2;
+                self.ys[i] = platform.CANVAS_SIZE / 2;
+                self.vxs[i] = rnd.random().intRangeAtMost(i8, -50, 50);
+                self.vys[i] = rnd.random().intRangeAtMost(i8, -50, 50);
+                self.spxs[i] = 0;
+                self.spys[i] = 0;
             }
-            i = 0;
+        }
+
+        fn update(self: *Self) void {
+            var i: u8 = 0;
             while (i < Self.n) : (i += 1) {
-                self.ys[i] = i * 3;
+                self.spxs[i] = self.spxs[i] + self.vxs[i];
+                self.xs[i] = @mod(self.xs[i] + @divTrunc(self.spxs[i], 100), platform.CANVAS_SIZE);
+                self.spxs[i] = @rem(self.spxs[i], 100);
+
+                self.spys[i] = self.spys[i] + self.vys[i];
+                self.ys[i] = @mod(self.ys[i] + @divTrunc(self.spys[i], 100), platform.CANVAS_SIZE);
+                self.spys[i] = @rem(self.spys[i], 100);
             }
         }
 
