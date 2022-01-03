0b54934efaed064e309a2d54a1d39adb6c255d1d
Jim Newsome <jim@sporksmith.net>
2022-01-03 16:27:22 -0600

    Update release script to bundle

--- particles/release.sh
+++ particles/release.sh
@@ -9,3 +9,5 @@ IN=zig-out/lib/cart.wasm
 OUT=zig-out/lib/release.wasm
 wasm-opt -Oz --strip-producers --strip-debug --dce --zero-filled-memory $IN -o $OUT -g
 
+DIRNAME=$(basename $(pwd))
+w4 bundle --html ../docs/$DIRNAME/index.html --description "https://github.com/sporksmith/wasm4-sketchbook/tree/main/$DIRNAME" zig-out/lib/release.wasm
